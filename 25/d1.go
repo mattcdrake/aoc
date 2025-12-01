74ac7dc63faef8bbabdba41a26c9560bec310b09
Matt Drake <mattdrakeworks@gmail.com>
2025-11-30 23:27:29 -0800

    refactor

--- 25/d1.go
+++ 25/d1.go
@@ -4,7 +4,7 @@ import (
 	"strconv"
 )
 
-func d1(lines []string) string {
+func d1(lines []string) int {
 	cur, res := 50, 0
 	for _, rotation := range lines {
 		cur, _ = rotate(cur, rotation)
@@ -12,24 +12,24 @@ func d1(lines []string) string {
 			res++
 		}
 	}
-	return strconv.Itoa(res)
+	return res
 }
 
-func d2(lines []string) string {
+func d2(lines []string) int {
 	cur, res, crosses := 50, 0, 0
 	for _, rotation := range lines {
 		cur, crosses = rotate(cur, rotation)
 		res += crosses
 	}
-	return strconv.Itoa(res)
+	return res
 }
 
-func rotate(cur int, move string) (int, int) {
+func rotate(init int, move string) (int, int) {
 	dist, _ := strconv.Atoi(move[1:])
-	init := cur
 	zeroes := dist / 100
 	dist %= 100
 
+	cur := init
 	if move[0] == 'R' {
 		cur += dist
 	} else {
